1aa028e9156e3207bf9dad1af2906e0d53348305
Sravan Pant <70965196+sravanpant@users.noreply.github.com>
2023-08-29 20:02:36 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Selection Sort/selection-sort.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution: 
+    def select(self, arr, i):
+        # code here 
+        return 0
+    
+    def selectionSort(self, arr,n):
+        #code here
+        for i in range(n):
+            min = i
+            for j in range(i+1, n):
+                if arr[j] < arr[min]:
+                    min = j
+            
+            temp = arr[min]
+            arr[min] = arr[i]
+            arr[i] = temp
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        Solution().selectionSort(arr, n)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
+# } Driver Code Ends
