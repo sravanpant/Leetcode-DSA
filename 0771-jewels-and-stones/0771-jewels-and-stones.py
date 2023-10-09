678305510b0615bc1873eb3622c109e1bb3ea950
Sravan Pant <70965196+sravanpant@users.noreply.github.com>
2023-10-09 22:09:57 +0530

    Time: 38 ms (63.39%), Space: 16.2 MB (47.72%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -6,7 +6,6 @@ class Solution:
         stones_dict = dict()
         for i in range(m):
             stones_dict[stones[i]] = stones_dict.get(stones[i], 0)+1
-        keys = stones_dict.keys()
         for j in range(n):
             for key, value in stones_dict.items():
                 if jewels[j] == key:
