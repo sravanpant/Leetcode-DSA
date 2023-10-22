01d4a2e4f0867d84741a8b3421473a993aad7d9a
Sravan Pant <70965196+sravanpant@users.noreply.github.com>
2023-10-23 01:48:21 +0530

    Time: 43 ms (42.98%), Space: 16.2 MB (97.25%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def solve(self, idx, seq, subseq, nums, n):
+        if idx == n:
+            print(subseq)
+            seq.append(subseq[:])
+            return
+
+        subseq.append(nums[idx])
+        self.solve(idx + 1, seq, subseq[:], nums, n)
+        subseq.pop()
+        self.solve(idx + 1, seq, subseq[:], nums, n)
+        return
+    
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subseq = []
+        seq = []
+        n = len(nums)
+        self.solve(0, seq, subseq, nums, n)
+        return seq
+        
+        
