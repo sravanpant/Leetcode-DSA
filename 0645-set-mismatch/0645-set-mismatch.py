d118536140d1eae4c6a759c4052da314fc32a064
Sravan Pant <70965196+sravanpant@users.noreply.github.com>
2024-01-22 14:18:39 +0530

    Time: 140 ms (99.40%), Space: 18.5 MB (57.50%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,14 +1,7 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        n=len(nums)
         n = len(nums)
-        dup, missing = -1, -1
-
-        for i in range(1, n + 1):
-            count = nums.count(i)
-            if count == 2:
-                dup = i
-            elif count == 0:
-                missing = i
-
-        return [dup, missing]
+        a = sum(nums)
+        b = sum(set(nums))
+        s = n * (n + 1) // 2
+        return [a - b, s - b]
